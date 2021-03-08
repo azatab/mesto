6ac21ba9f644ed8e185fea104b69ab2f3e5d2b6b
Azat Abrarov <ufasat@yandex.ru>
2021-03-08 19:43:33 +0500

    load cards via template tag

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,104 @@
-let editProfileButton = document.querySelector('#profile-edit')
-let closePopupButton = document.querySelector('.popup__close')
-let popup = document.querySelector('.popup');
+const initialCards = [
+  {name: 'Гора Эльбрус', link: '../images/elbrus2.jpg'},
+  {name: 'Уральские горы', link: '../images/ural-mnts.jpg'},
+  {name: 'Уфа', link: '../images/ufa.jpg'},
+  {name: 'Карачаево-Черкессия', link: '../images/elbrus3.jpg'},
+  {name: 'Памятник Салавату Юлаеву', link: '../images/ufa.jpg'},
+  {name: 'Уральские горы', link: '../images/ural-mnts.jpg'}
+];
+
+let editProfileButton = document.querySelector('#profile-edit');
+
+let addButton = document.querySelector('.profile__add-button');
+let popupEdit = document.querySelector('.popup-edit');
+let popupAdd = document.querySelector('.popup-add');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 let inputName = document.querySelector('#input-name');
 let inputJob = document.querySelector('#input-job');
-let formElement = document.querySelector('.form');
-let likeButton = document.querySelectorAll('.cards__like-button');
+let inputPlaceName = document.querySelector('#input-place-name');
+let inputImageLink = document.querySelector('#input-image-link');
+let formEditElement = document.querySelector('.edit-form');
+let formAddElement = document.querySelector('.add-form');
+const templateCard = document.querySelector('.template-card');
+const cardsContainer = document.querySelector('.cards__list');
 
-function formSubmitHandler (event) {
+function editFormSubmitHandler (event) {
   event.preventDefault();
   profileName.textContent =  inputName.value;
   profileJob.textContent = inputJob.value;
-  closePopupEdit();
+  closePopup(event);
+}
+
+function addFormSubmitHandler (event) {
+  event.preventDefault();
+  //profileName.textContent =  inputName.value;
+  //profileJob.textContent = inputJob.value;
+  closePopup(event);
 }
 
 function showPopupEdit () {
-  popup.classList.add('popup_is-opened');
+  popupEdit.classList.add('popup_is-opened');
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
 }
 
-function closePopupEdit () {
-  popup.classList.remove('popup_is-opened');
+function showPopupAdd () {
+  popupAdd.classList.add('popup_is-opened');
+}
+
+function closePopup (evt) {
+  const currentPopup = evt.target.closest('.popup');
+  currentPopup.classList.remove('popup_is-opened');
 }
 
 const toggleLike = (evt) => {
   evt.target.classList.toggle('cards__like-button-active');
 }
 
+function deleteCardHandler(evt) {
+  const target = evt.target;
+  const currentCard = target.closest('.cards__item');
+  currentCard.remove();
+}
+
+function createCardDomNode(item) {
+  const newCard = templateCard.content.cloneNode(true);
+  const title = newCard.querySelector('.cards__title');
+  const image = newCard.querySelector('.cards__image');
+  title.textContent = item.name;
+  image.src = item.link;
+  image.alt = item.name;
+
+  return newCard;
+}
+
+function renderInitialCards() {
+  const result = initialCards.map(function(item) {
+    const newCard = createCardDomNode(item);
+    return newCard;
+  });
+  cardsContainer.append(...result);
+}
+renderInitialCards();
+
 editProfileButton.addEventListener('click', showPopupEdit);
-closePopupButton.addEventListener('click', closePopupEdit);
-formElement.addEventListener('submit', formSubmitHandler); 
+
+addButton.addEventListener('click', showPopupAdd);
+formEditElement.addEventListener('submit', editFormSubmitHandler);
+formAddElement.addEventListener('submit', addFormSubmitHandler);
+
+const likeButton = document.querySelectorAll('.cards__like-button');
 likeButton.forEach(function (entry) {
   entry.addEventListener('click', toggleLike);
+});
+
+const deleteButton = document.querySelectorAll('.cards__delete-button');
+deleteButton.forEach(function(entry) {
+  entry.addEventListener('click', deleteCardHandler);
+});
+
+const closePopupButton = document.querySelectorAll('.popup__close');
+closePopupButton.forEach(function(entry) {
+  entry.addEventListener('click', closePopup);
 });
