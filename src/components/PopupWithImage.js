8f753709b0d9b6514c3190328fbebf78ed8106a9
Azat Abrarov <ufasat@yandex.ru>
2021-04-25 04:26:31 +0500

    fix consts to class properties

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,19 +1,16 @@
 import Popup from "./Popup.js";
 
-
-const image = document.querySelector('.popup-image__photo');
-const caption = document.querySelector('.popup-image__figcaption');
-
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector)
-
+    this._image = document.querySelector('.popup-image__photo')
+    this._caption = document.querySelector('.popup-image__figcaption')
   }
 
   open(item) {
-    image.src = item.link;
-    image.alt = item.name;
-    caption.textContent = item.name;
+    this._image.src = item.link
+    this._image.alt = item.name
+    this._caption.textContent = item.name
 
     super.open()
   }
